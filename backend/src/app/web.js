210a64a6fa89d60ccff3671d7a438cbcac99261e
nuhptr <nugrahaadi733@gmail.com>
2024-05-01 12:51:19 +0700

    web: add message routes

--- backend/src/app/web.js
+++ backend/src/app/web.js
@@ -2,10 +2,12 @@ import express from "express"
 import helmet from "helmet"
 
 import { authRouter } from "../routes/auth-routes.js"
+import { messageRouter } from "../routes/message-routes.js"
 
 export const web = express()
 web.use(helmet())
-web.use(express.json())
+web.use(express.json()) //* Parse JSON bodiess
 web.use(express.urlencoded({ extended: true }))
 
 web.use("/api/auth", authRouter)
+web.use("/api/messages", messageRouter)
