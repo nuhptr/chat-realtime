d6e4a13dfee8d3ae660972db1bed6ac6a37cd2e7
nuhptr <nugrahaadi733@gmail.com>
2024-05-01 12:21:20 +0700

    backend: src index update code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv"
-dotenv.config()
 
 import { web } from "../src/app/web.js"
+import { connectToDb } from "./lib/db.js"
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
+
+dotenv.config()
 
 web.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+   connectToDb()
+   console.log(`Server is running on port ${PORT}`)
 })
