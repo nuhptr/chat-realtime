2d0afd00b6e8d849bac874d2956822a46913ccf2
nuhptr <nugrahaadi733@gmail.com>
2024-05-01 12:52:24 +0700

    lib: generate JWT token

---
+++ backend/src/lib/generate-token.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken"
+
+export function generateTokenAndSetCookie(userId, res) {
+   const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" })
+
+   res.cookie("jwt", token, {
+      maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+      httpOnly: true, // Prevent XSS cookie access from client side JavaScript
+      sameSite: true, // CSRF protection
+      secure: process.env.NODE_ENV !== "development", // HTTPS only in production
+   })
+}
