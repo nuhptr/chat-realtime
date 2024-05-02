98a07233f5eec8ad22a50be61d4bb4d2f7aa3499
nuhptr <nugrahaadi733@gmail.com>
2024-05-02 13:22:24 +0700

    app: add dotenv and cookie-parser

--- backend/src/app/web.js
+++ backend/src/app/web.js
@@ -1,13 +1,19 @@
 import express from "express"
 import helmet from "helmet"
+import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
 
 import { authRouter } from "../routes/auth-routes.js"
 import { messageRouter } from "../routes/message-routes.js"
 
 export const web = express()
+
+dotenv.config()
+
 web.use(helmet())
 web.use(express.json()) //* Parse JSON bodiess
 web.use(express.urlencoded({ extended: true }))
+web.use(cookieParser())
 
 web.use("/api/auth", authRouter)
 web.use("/api/messages", messageRouter)
