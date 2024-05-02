c569c83e5707983fdc7802c014fa85f4d51bace5
nuhptr <nugrahaadi733@gmail.com>
2024-05-02 14:45:40 +0700

    controller: message/getmessage

--- backend/src/controller/message-controller.js
+++ backend/src/controller/message-controller.js
@@ -1,6 +1,7 @@
 import { Message } from "../model/message-model.js"
 import { Conversation } from "../model/conversation-model.js"
 
+//* SEND MESSAGE CONTROLLER
 export async function sendMessage(req, res) {
    try {
       const { message } = req.body
@@ -20,12 +21,39 @@ export async function sendMessage(req, res) {
          conversation.messages.push(newMessage._id)
       }
 
+      //* SOCKET IO FUNCTIONALITY WILL GO HERE
+
       //* This will save the message and conversation in parallel
       await Promise.all([newMessage.save(), conversation.save()])
 
       res.status(201).json(newMessage)
    } catch (error) {
       console.error("Error in Send Message Controller: ", error.message)
+      res.status(500).json({ message: "Internal Server Error." })
+   }
+}
+
+//* GET MESSAGES CONTROLLER
+export async function getMessages(req, res) {
+   try {
+      const { id: userChatToId } = req.params
+      const senderId = req.user._id
+
+      /**
+       * this will find the conversation between the sender and the receiver
+       * not reference to the messages but the actual messages
+       * */
+      const conversation = await Conversation.findOne({
+         participants: { $all: [senderId, userChatToId] },
+      }).populate("messages")
+
+      if (!conversation) return res.status(200).json([])
+
+      const messages = conversation.messages
+
+      res.status(200).json(messages)
+   } catch (error) {
+      console.error("Error in Get Messages Controller: ", error.message)
       res.status(500).json({ message: "Internal Server Error." })
    }
 }
