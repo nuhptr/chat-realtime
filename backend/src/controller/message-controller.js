4e4faa1edf1d78e6f0dc0b892c620de9becc0423
nuhptr <nugrahaadi733@gmail.com>
2024-05-02 13:23:59 +0700

    controller: message logic service

--- backend/src/controller/message-controller.js
+++ backend/src/controller/message-controller.js
@@ -1,1 +1,31 @@
-export async function sendMessage(req, res) {}
+import { Message } from "../model/message-model.js"
+import { Conversation } from "../model/conversation-model.js"
+
+export async function sendMessage(req, res) {
+   try {
+      const { message } = req.body
+      const { id: receiverId } = req.params
+      const senderId = req.user._id
+
+      let conversation = await Conversation.findOne({
+         participants: { $all: [senderId, receiverId] },
+      })
+
+      if (!conversation) {
+         conversation = await Conversation.create({ participants: [senderId, receiverId] })
+      }
+
+      const newMessage = new Message({ senderId, receiverId, message })
+      if (newMessage) {
+         conversation.messages.push(newMessage._id)
+      }
+
+      //* This will save the message and conversation in parallel
+      await Promise.all([newMessage.save(), conversation.save()])
+
+      res.status(201).json(newMessage)
+   } catch (error) {
+      console.error("Error in Send Message Controller: ", error.message)
+      res.status(500).json({ message: "Internal Server Error." })
+   }
+}
