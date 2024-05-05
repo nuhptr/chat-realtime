221f448503833b4d4d5a22f97e1953669b7b8e6c
nuhptr <nugrahaadi733@gmail.com>
2024-05-05 23:41:00 +0700

    components: gender checkbox

--- frontend/src/components/gender-checkbox.jsx
+++ frontend/src/components/gender-checkbox.jsx
@@ -5,7 +5,7 @@ export default function GenderCheckbox() {
       <div className="flex gap-x-2">
          <div className="form-control">
             <label htmlFor="male" className="gap-2 cursor-pointer label">
-               <span className="label-text">Male</span>
+               <span className="text-white label-text">Male</span>
                <input
                   type="checkbox"
                   name="male"
@@ -16,7 +16,7 @@ export default function GenderCheckbox() {
 
          <div className="form-control">
             <label htmlFor="female" className="gap-2 cursor-pointer label">
-               <span className="label-text">Female</span>
+               <span className="text-white label-text">Female</span>
                <input
                   type="checkbox"
                   className="border-white checkbox checked:border-rose-500 [--chkbg:theme(colors.rose.500)] [--chkfg:white]"
