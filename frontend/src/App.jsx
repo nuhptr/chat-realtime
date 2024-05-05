2c786f2cfb825290f7f32461388a1ab3876b4cc4
nuhptr <nugrahaadi733@gmail.com>
2024-05-05 23:59:22 +0700

    app: update code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,13 @@
 import React from "react"
 
+import LoginPages from "./pages/login"
+import SignUpPages from "./pages/signup"
+import HomePages from "./pages/home"
+
 export default function App() {
-   return <div>App</div>
+   return (
+      <div className="flex items-center justify-center h-screen p-4">
+         <HomePages />
+      </div>
+   )
 }
