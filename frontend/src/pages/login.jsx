f6f9b166b7e526d80aa9c42e5c36089eaebdb925
nuhptr <nugrahaadi733@gmail.com>
2024-05-05 23:59:11 +0700

    pages: auth done slicing

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -3,27 +3,27 @@ import React from "react"
 export default function LoginPages() {
    return (
       <div className="flex flex-col items-center justify-center mx-auto min-w-96">
-         <div className="w-full p-6 bg-gray-400 bg-opacity-0 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-lg">
-            <h1 className="text-3xl font-semibold text-center text-gray-300">
-               Login <span className="text-green-500">Messenger</span>
+         <div className="w-full p-6 bg-gray-400 bg-opacity-0 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-lg">
+            <h1 className="text-3xl font-semibold text-center text-white">
+               Login <span className="text-green-400">Messenger</span>
             </h1>
 
             <form action="">
-               <div className="mt-1 tracking-tight">
+               <div className="mt-2 tracking-tight">
                   <label htmlFor="username" className="p-2 label">
-                     <span className="text-base label-text">Username</span>
+                     <span className="text-base text-white label-text">Username</span>
                   </label>
                   <input
                      type="text"
                      name="username"
                      placeholder="Enter username."
-                     className="w-full h-10 input input-bordered"
+                     className="w-full input input-bordered"
                   />
                </div>
 
                <div className="mt-1 tracking-tight">
                   <label htmlFor="password" className="p-2 label">
-                     <span className="text-base label-text">Password</span>
+                     <span className="text-base text-white label-text">Password</span>
                   </label>
                   <input
                      type="password"
@@ -33,17 +33,17 @@ export default function LoginPages() {
                   />
                </div>
 
-               <a
-                  href="#"
-                  className="inline-block mt-2 text-sm hover:underline hover:text-green-600">
-                  {"Don't"} have an account? Register
-               </a>
-
                <div>
-                  <button className="w-full h-10 mt-2 text-white bg-green-600 border-none hover:bg-green-700 btn btn-sm">
+                  <button className="w-full mt-6 text-white bg-green-600 border-none hover:bg-green-700 btn btn-md">
                      Login
                   </button>
                </div>
+
+               <a
+                  href="#"
+                  className="inline-block w-full mt-2 text-sm text-center text-white hover:underline hover:text-green-600">
+                  {"Don't"} have an account? Register
+               </a>
             </form>
          </div>
       </div>
--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -4,61 +4,61 @@ import GenderCheckbox from "../components/gender-checkbox"
 export default function SignUpPages() {
    return (
       <div className="flex flex-col items-center justify-center mx-auto min-w-96">
-         <div className="w-full p-6 bg-gray-400 bg-opacity-0 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-lg">
-            <h1 className="text-3xl font-semibold text-center text-gray-300">
-               Sign Up <span className="text-green-500">Messenger</span>
+         <div className="w-full p-6 bg-gray-400 bg-opacity-0 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-lg">
+            <h1 className="text-3xl font-semibold text-center text-white">
+               Sign Up <span className="text-green-400">Messenger</span>
             </h1>
 
             <form action="">
-               <div className="mt-1 tracking-tight">
+               <div className="mt-2 tracking-tight">
                   <label htmlFor="full-name" className="p-2 label">
-                     <span className="text-base label-text">Full Name</span>
+                     <span className="text-base text-white label-text">Full Name</span>
                   </label>
                   <input
                      type="text"
                      name="full-name"
                      placeholder="Jhon Doe"
-                     className="w-full h-10 input input-bordered"
+                     className="w-full input input-bordered"
                   />
                </div>
 
                <div className="mt-1 tracking-tight">
                   <label htmlFor="username" className="p-2 label">
-                     <span className="text-base label-text">Username</span>
+                     <span className="text-base text-white label-text">Username</span>
                   </label>
                   <input
                      type="text"
                      name="username"
                      placeholder="jhondoe"
-                     className="w-full h-10 input input-bordered"
+                     className="w-full input input-bordered"
                   />
                </div>
 
                <div className="mt-1 mb-2 tracking-tight">
                   <label htmlFor="password" className="p-2 label">
-                     <span className="text-base label-text">Password</span>
+                     <span className="text-base text-white label-text">Password</span>
                   </label>
                   <input
                      type="text"
                      name="password"
                      placeholder="*******"
-                     className="w-full h-10 input input-bordered"
+                     className="w-full input input-bordered"
                   />
                </div>
 
                <GenderCheckbox />
 
-               <a
-                  href="#"
-                  className="inline-block mt-2 text-sm text-white hover:underline hover:text-green-600">
-                  Already Have an Account? Login
-               </a>
-
                <div>
-                  <button className="w-full h-10 mt-2 text-white bg-green-600 border-none hover:bg-green-700 btn btn-sm">
+                  <button className="w-full mt-6 text-white bg-green-600 border-none hover:bg-green-700 btn btn-md">
                      Register
                   </button>
                </div>
+
+               <a
+                  href="#"
+                  className="inline-block w-full mt-2 text-sm text-center text-white hover:underline hover:text-green-600">
+                  Already Have an Account? Login
+               </a>
             </form>
          </div>
       </div>
