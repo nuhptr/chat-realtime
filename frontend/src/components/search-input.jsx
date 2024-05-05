de6f75b7577892c2408a9777bd2b1d509a2c57b4
nuhptr <nugrahaadi733@gmail.com>
2024-05-05 23:41:18 +0700

    components: search input add

---
+++ frontend/src/components/search-input.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { IoSearchSharp } from "react-icons/io5"
+
+export default function SearchInput() {
+   return (
+      <form className="flex items-center gap-2">
+         <input type="text" placeholder="search.." className="rounded-full input input-bordered" />
+         <button
+            type="submit"
+            className="text-white bg-green-500 btn btn-circle hover:bg-green-600">
+            <IoSearchSharp className="w-6 h-6 outline-none" />
+         </button>
+      </form>
+   )
+}
