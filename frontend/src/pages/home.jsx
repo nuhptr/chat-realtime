c5d8623bef881ac59410ab3f9791ad9b2e1ad32d
nuhptr <nugrahaadi733@gmail.com>
2024-05-05 23:59:34 +0700

    pages: home sidebar slicing first

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,5 +1,13 @@
 import React from "react"
 
+import Sidebar from "../components/sidebar"
+import MessageContainer from "../components/message-container"
+
 export default function HomePages() {
-   return <div>Home Pages</div>
+   return (
+      <div className="flex sm:h-[450px] md:h-[550px] rounded-2xl overflow-hidden bg-white bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+         <Sidebar />
+         {/* <MessageContainer /> */}
+      </div>
+   )
 }
