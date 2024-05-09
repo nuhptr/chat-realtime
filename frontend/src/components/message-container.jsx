4064327995522f214ce69b2d9863adc13e4a437f
nuhptr <nugrahaadi733@gmail.com>
2024-05-09 12:56:53 +0700

    components: message components done

--- frontend/src/components/message-container.jsx
+++ frontend/src/components/message-container.jsx
@@ -1,19 +1,40 @@
+import { TbAlignBoxLeftMiddle } from "react-icons/tb"
+
 import Messages from "./messages"
 import MessageInput from "./message-input"
 
 export default function MessageContainer() {
+   const noChatSelected = true
+
    return (
       <div className="md:min-w-[450px] flex flex-col py-8 pr-8 max-sm:py-4 max-sm:pr-4">
-         <>
-            {/* Header */}
-            <div className="px-5 py-3 bg-white border border-white rounded-full border-opacity-30 bg-opacity-10 backdrop-blur-lg backdrop-filter">
-               <span className="text-white label-text">To: </span>{" "}
-               <span className="font-bold text-white ">Jhon Doe</span>
-            </div>
+         {noChatSelected && <NoChatSelected />}
+         {!noChatSelected && (
+            <>
+               {/* Header */}
+               <div className="px-5 py-3 bg-white border border-white rounded-full border-opacity-30 bg-opacity-10 backdrop-blur-lg backdrop-filter">
+                  <span className="text-white label-text">To: </span>{" "}
+                  <span className="font-bold text-white ">Jhon Doe</span>
+               </div>
 
-            <Messages />
-            <MessageInput />
-         </>
+               <Messages />
+               <MessageInput />
+            </>
+         )}
+      </div>
+   )
+}
+
+const NoChatSelected = () => {
+   return (
+      <div className="flex items-center justify-center w-full h-full">
+         <div className="flex flex-col items-center gap-2 px-4 font-semibold text-center text-gray-200 sm:text-lg md:text-xl">
+            <TbAlignBoxLeftMiddle className="text-3xl text-center md:text-6xl" />
+            <p className="text-2xl tracking-tight">Welcome 🚀 Jhon Doe</p>
+            <p className="text-base font-normal tracking-tight">
+               Select a chat to start messaging now.
+            </p>
+         </div>
       </div>
    )
 }
--- frontend/src/components/message-input.jsx
+++ frontend/src/components/message-input.jsx
@@ -1,11 +1,17 @@
+import { BsArrowRightCircleFill } from "react-icons/bs"
+
 export default function MessageInput() {
    return (
       <form className="px-4 my-3">
-         <div className="w-full">
+         <div className="relative w-full">
             <input
                type="text"
-               className="block w-full p-4 text-sm text-black bg-white border border-gray-300 rounded-2xl bg-opacity-20 backdrop-filter backdrop-blur-lg active:outline-none focus:outline-none focus:ring-0 focus:border-white"
+               className="block w-full p-4 text-sm text-black bg-white border border-gray-300 rounded-2xl bg-opacity-20 backdrop-filter backdrop-blur-lg active:outline-none focus:outline-none focus:ring-0 focus:border-white placeholder:text-black placeholder:text-opacity-30"
+               placeholder="Type a message.."
             />
+            <button type="submit" className="absolute inset-y-0 flex items-center end-0 pe-3">
+               <BsArrowRightCircleFill className="w-6 h-6 text-white" />
+            </button>
          </div>
       </form>
    )
