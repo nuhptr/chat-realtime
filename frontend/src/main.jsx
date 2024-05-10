e15fd7e29171907be0aa24f03c456e3602888b67
nuhptr <nugrahaadi733@gmail.com>
2024-05-10 13:08:01 +0700

    main: add browserRouter and AuthContextProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,18 @@
 import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 
 import "./index.css"
 import App from "./App.jsx"
+import { AuthContextProvider } from "./context/auth-context.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <StrictMode>
-      <App />
+      {/* Apply Browser Router For Routing React App */}
+      <BrowserRouter>
+         <AuthContextProvider>
+            <App />
+         </AuthContextProvider>
+      </BrowserRouter>
    </StrictMode>
 )
