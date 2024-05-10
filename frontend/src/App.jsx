8439bdd5a625541c2f8c761ccee3e7f2179bd296
nuhptr <nugrahaadi733@gmail.com>
2024-05-10 13:08:16 +0700

    app: add list router and toaster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
+import { Routes, Route } from "react-router-dom"
+import { Toaster } from "react-hot-toast"
+
+import HomePages from "./pages/home"
 import LoginPages from "./pages/login"
 import SignUpPages from "./pages/signup"
-import HomePages from "./pages/home"
 
 export default function App() {
    return (
       <div className="flex items-center justify-center h-screen p-4">
-         <HomePages />
+         {/* Apply List Route */}
+         <Routes>
+            <Route path="/" element={<HomePages />} />
+            <Route path="/login" element={<LoginPages />} />
+            <Route path="/signup" element={<SignUpPages />} />
+         </Routes>
+         <Toaster />
       </div>
    )
 }
