708852a234bec1718f25b1ebb87f4f5121be838f
nuhptr <nugrahaadi733@gmail.com>
2024-05-10 13:08:54 +0700

    component: genderCheckbox udpate code

--- frontend/src/components/gender-checkbox.jsx
+++ frontend/src/components/gender-checkbox.jsx
@@ -1,23 +1,33 @@
-export default function GenderCheckbox() {
+export default function GenderCheckbox({ onCheckboxChange, selectedGender }) {
    return (
       <div className="flex gap-x-2">
          <div className="form-control">
-            <label htmlFor="male" className="gap-2 cursor-pointer label">
+            <label
+               htmlFor="male"
+               className={`gap-2 cursor-pointer label 
+               ${selectedGender === "male" ? "selected" : ""}`}>
                <span className="text-white label-text">Male</span>
                <input
                   type="checkbox"
                   name="male"
                   className="border-white checkbox checked:border-green-500 [--chkbg:theme(colors.green.500)] [--chkfg:white]"
+                  checked={selectedGender === "male"}
+                  onChange={() => onCheckboxChange("male")}
                />
             </label>
          </div>
 
          <div className="form-control">
-            <label htmlFor="female" className="gap-2 cursor-pointer label">
+            <label
+               htmlFor="female"
+               className={`gap-2 cursor-pointer label 
+               ${selectedGender === "female" ? "selected" : ""}`}>
                <span className="text-white label-text">Female</span>
                <input
                   type="checkbox"
                   className="border-white checkbox checked:border-rose-500 [--chkbg:theme(colors.rose.500)] [--chkfg:white]"
+                  checked={selectedGender === "female"}
+                  onChange={() => onCheckboxChange("female")}
                />
             </label>
          </div>
