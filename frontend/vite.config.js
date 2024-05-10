0736ffb05677b6c40b5a744d5b14e17e21fedf6a
nuhptr <nugrahaadi733@gmail.com>
2024-05-10 13:07:41 +0700

    vite: add server configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
+   server: {
+      // To run the server on port 3000
+      port: 3000,
+      proxy: {
+         // Proxy /api/auth/signup to http://localhost:8000/api/auth/signup
+         "/api": {
+            target: "http://localhost:8000",
+            changeOrigin: true,
+         },
+      },
+   },
 })
