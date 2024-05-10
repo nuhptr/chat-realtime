46fe2b9d386667e704ba73ee6ba3f730d892c2a1
nuhptr <nugrahaadi733@gmail.com>
2024-05-10 13:12:15 +0700

    component: logout button update code

--- frontend/src/components/logout-button.jsx
+++ frontend/src/components/logout-button.jsx
@@ -2,11 +2,13 @@ import { BsFillArrowLeftSquareFill } from "react-icons/bs"
 
 export default function LogoutButton() {
    return (
-      <div className="flex items-center py-4 mt-auto group gap-x-3">
-         <BsFillArrowLeftSquareFill className="w-6 h-6 cursor-pointer text-rose-500 group-hover:text-rose-600" />{" "}
-         <span className="text-lg tracking-tight text-white group-hover:text-rose-500 group-hover:cursor-pointer">
-            Logout
-         </span>
+      <div className="py-4 mt-auto group">
+         <a href="/login" className="flex items-center gap-x-3">
+            <BsFillArrowLeftSquareFill className="w-6 h-6 cursor-pointer text-rose-500 group-hover:text-rose-600" />{" "}
+            <span className="text-lg tracking-tight text-white group-hover:text-rose-500 group-hover:cursor-pointer">
+               Logout
+            </span>
+         </a>
       </div>
    )
 }
