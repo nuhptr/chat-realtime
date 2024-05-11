31292bb73f8e2b7c7d10c9ac5381393637e02237
nuhptr <nugrahaadi733@gmail.com>
2024-05-11 21:35:41 +0700

    main: add socketContextProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,18 @@ import { BrowserRouter } from "react-router-dom"
 
 import "./index.css"
 import App from "./App.jsx"
+
 import { AuthContextProvider } from "./context/auth-context.jsx"
+import { SocketContextProvider } from "./context/socket-context.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <StrictMode>
       {/* Apply Browser Router For Routing React App */}
       <BrowserRouter>
          <AuthContextProvider>
-            <App />
+            <SocketContextProvider>
+               <App />
+            </SocketContextProvider>
          </AuthContextProvider>
       </BrowserRouter>
    </StrictMode>
