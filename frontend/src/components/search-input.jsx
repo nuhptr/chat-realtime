a5a2d6630272458476082c16edf6ab917b76bc8d
nuhptr <nugrahaadi733@gmail.com>
2024-05-11 21:38:05 +0700

    components: search input update code

--- frontend/src/components/search-input.jsx
+++ frontend/src/components/search-input.jsx
@@ -1,9 +1,43 @@
+import { useState } from "react"
 import { IoSearchSharp } from "react-icons/io5"
 
+import { useConversationState } from "../zustand"
+import { useGetConversation } from "../hooks/use-get-conversation"
+import toast from "react-hot-toast"
+
 export default function SearchInput() {
+   const [search, setSearch] = useState()
+   const { setSelectedConversation } = useConversationState()
+   const { conversations } = useGetConversation()
+
+   const handleSubmit = (e) => {
+      e.preventDefault()
+      if (!search) return
+      if (search.length < 3) {
+         return toast.error("Search must at least 3 characters long")
+      }
+
+      const conversation = conversations.find((c) =>
+         c.fullName.toLowerCase().includes(search.toLowerCase())
+      )
+
+      if (conversation) {
+         setSelectedConversation(conversation)
+         setSearch("")
+      } else {
+         toast.error("Username not found")
+      }
+   }
+
    return (
-      <form className="flex items-center gap-2">
-         <input type="text" placeholder="search.." className="rounded-full input input-bordered" />
+      <form onSubmit={handleSubmit} className="flex items-center gap-2">
+         <input
+            type="text"
+            placeholder="search.."
+            className="rounded-full input input-bordered"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+         />
          <button
             type="submit"
             className="text-white bg-green-500 btn btn-circle hover:bg-green-600">
