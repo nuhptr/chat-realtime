faf1d8e20bc2004c717978b062e6c06bb8e51951
nuhptr <nugrahaadi733@gmail.com>
2024-05-11 21:32:46 +0700

    index delete

--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-import { server } from "../src/socket/index.js"
-import { connectToDb } from "./lib/db.js"
-
-server.listen(process.env.PORT, () => {
-   connectToDb()
-   console.log(`Server is running on port ${process.env.PORT}`)
-})
