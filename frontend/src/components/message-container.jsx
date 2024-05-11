61d0e281cfa7410c3f2308c1622ce491f68bdeda
nuhptr <nugrahaadi733@gmail.com>
2024-05-11 21:36:47 +0700

    components: message container add authContext and global state

--- frontend/src/components/message-container.jsx
+++ frontend/src/components/message-container.jsx
@@ -1,20 +1,29 @@
+import { useEffect } from "react"
 import { TbAlignBoxLeftMiddle } from "react-icons/tb"
+
+import { useConversationState } from "../zustand"
+import { useAuthContext } from "../context/auth-context"
 
 import Messages from "./messages"
 import MessageInput from "./message-input"
 
 export default function MessageContainer() {
-   const noChatSelected = true
+   const { selectedConversation, setSelectedConversation } = useConversationState()
+
+   useEffect(() => {
+      // Cleanup function (unmounting)
+      return () => setSelectedConversation(null)
+   }, [setSelectedConversation])
 
    return (
       <div className="md:min-w-[450px] flex flex-col py-8 pr-8 max-sm:py-4 max-sm:pr-4">
-         {noChatSelected && <NoChatSelected />}
-         {!noChatSelected && (
+         {!selectedConversation && <NoChatSelected />}
+         {selectedConversation && (
             <>
                {/* Header */}
                <div className="px-5 py-3 bg-white border border-white rounded-full border-opacity-30 bg-opacity-10 backdrop-blur-lg backdrop-filter">
                   <span className="text-white label-text">To: </span>{" "}
-                  <span className="font-bold text-white ">Jhon Doe</span>
+                  <span className="font-bold text-white ">{selectedConversation.fullName}</span>
                </div>
 
                <Messages />
@@ -26,11 +35,13 @@ export default function MessageContainer() {
 }
 
 const NoChatSelected = () => {
+   const { authUser } = useAuthContext()
+
    return (
       <div className="flex items-center justify-center w-full h-full">
          <div className="flex flex-col items-center gap-2 px-4 font-semibold text-center text-gray-200 sm:text-lg md:text-xl">
             <TbAlignBoxLeftMiddle className="text-3xl text-center md:text-6xl" />
-            <p className="text-2xl tracking-tight">Welcome 🚀 Jhon Doe</p>
+            <p className="text-2xl tracking-tight">Welcome 🚀 {authUser.fullName}</p>
             <p className="text-base font-normal tracking-tight">
                Select a chat to start messaging now.
             </p>
