5bbe10048c93706b0c914b15a38b832f123dce0f
nuhptr <nugrahaadi733@gmail.com>
2024-05-11 21:35:24 +0700

    app: add authContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import { Toaster } from "react-hot-toast"
 
 import HomePages from "./pages/home"
 import LoginPages from "./pages/login"
 import SignUpPages from "./pages/signup"
 
+import { useAuthContext } from "./context/auth-context"
+
 export default function App() {
+   const { authUser } = useAuthContext()
+
+   // Apply list routes and add authUser to check if user is logged in or not
    return (
       <div className="flex items-center justify-center h-screen p-4">
-         {/* Apply List Route */}
          <Routes>
-            <Route path="/" element={<HomePages />} />
-            <Route path="/login" element={<LoginPages />} />
-            <Route path="/signup" element={<SignUpPages />} />
+            <Route path="/" element={authUser ? <HomePages /> : <Navigate to="/login" />} />
+            <Route path="/login" element={authUser ? <Navigate to="/" /> : <LoginPages />} />
+            <Route path="/signup" element={authUser ? <Navigate to="/" /> : <SignUpPages />} />
          </Routes>
          <Toaster />
       </div>
