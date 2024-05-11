04a83e0ee9ebbf9e7962c63591349606a92f8841
nuhptr <nugrahaadi733@gmail.com>
2024-05-11 13:55:26 +0700

    index: add server from scoket io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
-import { web } from "../src/app/web.js"
+import { server } from "../src/socket/index.js"
 import { connectToDb } from "./lib/db.js"
 
-web.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
    connectToDb()
    console.log(`Server is running on port ${process.env.PORT}`)
 })
