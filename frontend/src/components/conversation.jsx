b77a93f976a6bb6996a6eca13239e6e0acf810e8
nuhptr <nugrahaadi733@gmail.com>
2024-05-11 21:36:07 +0700

    components: conversation/(s) update code

--- frontend/src/components/conversation.jsx
+++ frontend/src/components/conversation.jsx
@@ -1,11 +1,27 @@
-export default function Conversation() {
+import { useConversationState } from "../zustand"
+import { useSocketContext } from "../context/socket-context"
+
+export default function Conversation({ conversation, emoji, lastIndex }) {
+   const { selectedConversation, setSelectedConversation } = useConversationState()
+
+   const isSelected = selectedConversation?._id === conversation._id
+
+   const { onlineUsers } = useSocketContext()
+   const isOnline = onlineUsers.includes(conversation._id)
+
    return (
       <>
-         <div className="flex items-center gap-2 px-2 py-2 cursor-pointer rounded-xl hover:bg-green-500">
-            <div className="avatar online">
+         <div
+            className={`flex items-center gap-2 px-2 py-2 cursor-pointer rounded-xl hover:bg-green-500 
+            ${isSelected ? "bg-green-500" : ""}`}
+            onClick={() => setSelectedConversation(conversation)}>
+            <div className={`avatar ${isOnline ? "online" : ""}`}>
                <div className="w-12 rounded-full">
                   <img
-                     src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
+                     src={
+                        conversation.profilePic ||
+                        "https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
+                     }
                      alt="User Avatar"
                   />
                </div>
@@ -13,13 +29,15 @@ export default function Conversation() {
 
             <div className="flex flex-col flex-1">
                <div className="flex justify-between gap-3">
-                  <p className="text-base font-semibold tracking-tight text-gray-200">Jhon Doe</p>
-                  <span className="text-xl">⛳️</span>
+                  <p className="text-base font-semibold tracking-tight text-gray-200">
+                     {conversation.fullName}
+                  </p>
+                  <span className="text-xl">{emoji}</span>
                </div>
             </div>
          </div>
 
-         <div className="w-full pt-[0.5px] opacity-30 rounded-full my-0 bg-white" />
+         {!lastIndex && <div className="w-full pt-[0.5px] opacity-30 rounded-full my-0 bg-white" />}
       </>
    )
 }
--- frontend/src/components/conversations.jsx
+++ frontend/src/components/conversations.jsx
@@ -1,16 +1,24 @@
+import { getRandomEmoji } from "../utils/emojis"
+import { useGetConversation } from "../hooks/use-get-conversation"
+
 import Conversation from "./conversation"
 
 export default function Conversations() {
+   // { fullName, gender, _id, username, profilePic, createdAt, updatedAt, __v }
+   const { loading, conversations } = useGetConversation()
+
    return (
       <div className="flex flex-col pt-8 overflow-auto gap-y-2 no-scrollbar">
-         <Conversation />
-         <Conversation />
-         <Conversation />
-         <Conversation />
-         <Conversation />
-         <Conversation />
-         <Conversation />
-         <Conversation />
+         {conversations.map((conversation, index) => (
+            <Conversation
+               key={conversation._id}
+               conversation={conversation}
+               emoji={getRandomEmoji()}
+               lastIndex={index === conversations.length - 1}
+            />
+         ))}
+
+         {loading ? <span className="loading loading-spinner"></span> : null}
       </div>
    )
 }
