374e2f5f67b6d95ac949e082e79e0dbfcbecf465
nuhptr <nugrahaadi733@gmail.com>
2024-05-11 21:37:49 +0700

    components: message/(s) update code

--- frontend/src/components/message.jsx
+++ frontend/src/components/message.jsx
@@ -1,17 +1,33 @@
-export default function Message() {
+import { useAuthContext } from "../context/auth-context"
+import { useConversationState } from "../zustand"
+import { FormatTime } from "../utils/format-time"
+
+export default function Message({ message }) {
+   const { authUser } = useAuthContext()
+   const { selectedConversation } = useConversationState()
+
+   const isSender = message.senderId === authUser._id
+
+   const chatClassname = isSender ? "chat-end" : "chat-start"
+   const profilePic = isSender ? authUser.profilePic : selectedConversation?.profilePic
+   const bubbleBgColor = isSender ? "bg-green-500 text-white" : "bg-gray-300 text-black"
+
+   const formattedTime = FormatTime(message.createdAt)
+
+   const shakeClass = message.shouldShake ? "shake" : ""
+
    return (
-      <div className="chat chat-end gap-y-2">
+      <div className={`chat gap-y-2 ${chatClassname}`}>
          <div className="chat-image avatar">
             <div className="w-10 rounded-full">
-               <img
-                  src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
-                  alt="Tailwind css chat buuble component"
-               />
+               <img src={profilePic || ""} alt="user profile picture" />
             </div>
          </div>
 
-         <div className="text-white bg-green-500 chat-bubble">Hi! What&apos;s up!</div>
-         <div className="flex items-center text-xs text-white opacity-50 chat-footer">12.42</div>
+         <div className={`chat-bubble ${bubbleBgColor} ${shakeClass}`}>{message.message}</div>
+         <div className="flex items-center text-xs text-white opacity-50 chat-footer">
+            {formattedTime}
+         </div>
       </div>
    )
 }
--- frontend/src/components/messages.jsx
+++ frontend/src/components/messages.jsx
@@ -1,16 +1,40 @@
+import { useEffect, useRef } from "react"
+
+import { useGetMessage } from "../hooks/use-get-message"
+
 import Message from "./message"
+import MessageSkeleton from "./ui/message-skeleton"
+import { useListenMessages } from "../hooks/use-listen-messages"
 
 export default function Messages() {
+   // {_id, senderId, receiverId, message, createdAt, updatedAt, __v}
+   const { loading, messages } = useGetMessage()
+   useListenMessages() // Custom hook to listen to new messages
+   const lastMessageRef = useRef()
+
+   useEffect(() => {
+      setTimeout(() => {
+         lastMessageRef.current?.scrollIntoView({ behavior: "smooth" })
+      }, 100)
+   }, [messages])
+
    return (
       <div className="flex-1 py-6 overflow-auto no-scrollbar">
-         <Message />
-         <Message />
-         <Message />
-         <Message />
-         <Message />
-         <Message />
-         <Message />
-         <Message />
+         {!loading &&
+            messages.length > 0 &&
+            messages.map((message) => (
+               <div key={message._id} ref={lastMessageRef}>
+                  <Message message={message} />
+               </div>
+            ))}
+
+         {loading && [...Array(3)].map((_, index) => <MessageSkeleton key={index} />)}
+
+         {!loading && messages.length === 0 && (
+            <p className="flex items-center justify-center h-full tracking-tight text-white">
+               Send a message to start the conversation
+            </p>
+         )}
       </div>
    )
 }
