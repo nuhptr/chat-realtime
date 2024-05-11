7735b0397267fa0eae8cc38ce1fa12d9a29b7ee6
nuhptr <nugrahaadi733@gmail.com>
2024-05-11 21:39:02 +0700

    pages: login update code

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
+
+import { useLogin } from "../hooks/use-login"
 
 export default function LoginPages() {
+   const [username, setUsername] = useState("")
+   const [password, setPassword] = useState("")
+
+   const { loading, login } = useLogin()
+
+   const handleSubmit = async (e) => {
+      e.preventDefault()
+      await login(username, password)
+   }
+
    return (
       <div className="flex flex-col items-center justify-center mx-auto min-w-96">
          <div className="w-full p-6 bg-gray-400 bg-opacity-0 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-lg">
@@ -8,7 +21,7 @@ export default function LoginPages() {
                Login <span className="text-green-400">Messenger</span>
             </h1>
 
-            <form action="">
+            <form onSubmit={handleSubmit}>
                <div className="mt-2 tracking-tight">
                   <label htmlFor="username" className="p-2 label">
                      <span className="text-base text-white label-text">Username</span>
@@ -18,6 +31,8 @@ export default function LoginPages() {
                      name="username"
                      placeholder="Enter username."
                      className="w-full input input-bordered"
+                     value={username}
+                     onChange={(e) => setUsername(e.target.value)}
                   />
                </div>
 
@@ -30,12 +45,17 @@ export default function LoginPages() {
                      name="password"
                      placeholder="Enter Password"
                      className="w-full input input-bordered"
+                     value={password}
+                     onChange={(e) => setPassword(e.target.value)}
                   />
                </div>
 
                <div>
-                  <button className="w-full mt-6 text-white bg-green-600 border-none hover:bg-green-700 btn btn-md">
-                     Login
+                  <button
+                     className="w-full mt-6 text-white bg-green-600 border-none hover:bg-green-700 btn btn-md"
+                     type="submit"
+                     disabled={loading}>
+                     {loading ? <span className="loading loading-spinner"></span> : "Login"}
                   </button>
                </div>
 
