ccf8946ed2329391f66fa9ff99b7eb4118e4dad2
nuhptr <nugrahaadi733@gmail.com>
2024-05-11 21:36:21 +0700

    components: logout button implemented useLogout

--- frontend/src/components/logout-button.jsx
+++ frontend/src/components/logout-button.jsx
@@ -1,14 +1,22 @@
 import { BsFillArrowLeftSquareFill } from "react-icons/bs"
 
+import { useLogout } from "../hooks/use-logout"
+
 export default function LogoutButton() {
+   const { loading, logout } = useLogout()
+
    return (
       <div className="py-4 mt-auto group">
-         <a href="/login" className="flex items-center gap-x-3">
-            <BsFillArrowLeftSquareFill className="w-6 h-6 cursor-pointer text-rose-500 group-hover:text-rose-600" />{" "}
-            <span className="text-lg tracking-tight text-white group-hover:text-rose-500 group-hover:cursor-pointer">
-               Logout
-            </span>
-         </a>
+         {!loading ? (
+            <a href="/login" className="flex items-center gap-x-3" onClick={logout}>
+               <BsFillArrowLeftSquareFill className="w-6 h-6 cursor-pointer text-rose-500 group-hover:text-rose-600" />{" "}
+               <span className="text-lg tracking-tight text-white group-hover:text-rose-500 group-hover:cursor-pointer">
+                  Logout
+               </span>
+            </a>
+         ) : (
+            <span className="loading loading-spinner"></span>
+         )}
       </div>
    )
 }
