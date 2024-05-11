01f93314e33475c659a596c36dbfdcef8d951ebc
nuhptr <nugrahaadi733@gmail.com>
2024-05-11 21:32:56 +0700

    app: starting point app

--- backend/src/app/web.js
+++ backend/src/app/web.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import cookieParser from "cookie-parser"
 import dotenv from "dotenv"
@@ -7,9 +8,14 @@ import { authRouter } from "../routes/auth-routes.js"
 import { messageRouter } from "../routes/message-routes.js"
 import { userRouter } from "../routes/user-routes.js"
 
-import { app } from "./socket/index.js"
+import { connectToDb } from "../lib/db.js"
+import { app, server } from "../lib/socket.js"
 
 dotenv.config()
+
+const __dirname = path.resolve()
+
+const PORT = process.env.PORT || 8000
 
 app.use(cors())
 app.use(express.json()) //* Parse JSON bodiess
@@ -18,3 +24,10 @@ app.use(cookieParser())
 app.use("/api/auth", authRouter)
 app.use("/api/messages", messageRouter)
 app.use("/api/users", userRouter)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+server.listen(PORT, function () {
+   connectToDb()
+   console.log(`Server is running on port ${PORT}`)
+})
